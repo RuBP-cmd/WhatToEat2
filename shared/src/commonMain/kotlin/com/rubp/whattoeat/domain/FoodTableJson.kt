c63df6125aa0344213fd26d3f8b879e992fff28c
RuBP <R810@foxmail.com>
2026-07-11 00:59:13 +0800

    完成FoodTableJson，允许List<Food>和FoodTable直接转化为jsonStr，允许jsonStr转化为FoodTableDto（会抛出异常，ui层需要根据异常显示提示信息）

--- shared/src/commonMain/kotlin/com/rubp/whattoeat/domain/FoodTableJson.kt
+++ shared/src/commonMain/kotlin/com/rubp/whattoeat/domain/FoodTableJson.kt
@@ -3,16 +3,28 @@ package com.rubp.whattoeat.domain
 import com.rubp.whattoeat.data.local.entry.Food
 import com.rubp.whattoeat.data.local.entry.FoodTable
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
-//@Serializable
-//data class FoodTableDto(val name)
-//
-//data class FoodDto(val name: String, val weight: Int) {
-//    fun createFood(): Food {
-//        return Food(name = name, weight = weight, marked = true)
-//    }
-//}
-//
-//fun foodTableToJson(foodTable: FoodTable, foods: List<Food>): String {
-//
-//}
+
+@Serializable
+data class FoodDto(val name: String, val weight: Int) // createdAt和tableId，以及marked需要其他地方分配
+@Serializable
+data class FoodTableDto(val name: String, val foodDtos:List<FoodDto>) // createdAt和tableId需要其他地方分配
+
+fun Food.toFoodDto(): FoodDto {
+    return FoodDto(this.name, this.weight)
+}
+
+fun foodTableToJson(foodTable: FoodTable, foods: List<Food>): String {
+    val foodDtos = foods.map { it.toFoodDto()}
+    val fooTableDto = FoodTableDto(foodTable.name, foodDtos)
+    return Json.encodeToString(fooTableDto)
+}
+
+// 会抛出异常
+@Throws(SerializationException::class, IllegalArgumentException::class)
+fun jsonToFoodTableDto(jsonStr: String): FoodTableDto {
+    return Json.decodeFromString<FoodTableDto>(jsonStr)
+}
